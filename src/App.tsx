859065a0823e3ef61cd4f0aae0e4bd2eeedcb56a
alexander.grishin <alexander.grishin@tunneltech.eu>
2021-05-15 00:47:10 +0300

    add for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={"/ecoeco"}>
             <AppRoutes />
         </BrowserRouter>
     );
